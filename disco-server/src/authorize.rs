9fc377d32b464d7a00867eb55966f8ef5693a5dc
izarma <phuckuhh@gmail.com>
2025-10-02 01:25:47 +0530

    disco server update

--- disco-server/src/authorize.rs
+++ disco-server/src/authorize.rs
@@ -6,11 +6,8 @@ use oauth2::{
 use reqwest::StatusCode;
 use serde::Deserialize;
 
-use crate::{
-    SharedCache, db,
-    module_bindings::set_name,
-    secret::{APP_SECRET, APPLICATION_ID},
-};
+use crate::SharedCache;
+use std::env;
 
 #[derive(Deserialize)]
 pub struct AuthResponse {
@@ -18,8 +15,15 @@ pub struct AuthResponse {
     state: String,
 }
 
-#[derive(Deserialize)]
-struct DiscordUser {
+#[derive(Deserialize, Debug)]
+struct GuildMember {
+    roles: Vec<String>,
+    user: DiscordUserInfo,
+}
+
+#[derive(Deserialize, Debug)]
+struct DiscordUserInfo {
+    id: String,
     username: String,
 }
 
@@ -27,58 +31,60 @@ pub(crate) async fn disco_auth(
     State(state): State<SharedCache>,
     query: Query<AuthResponse>,
 ) -> Result<String, StatusCode> {
-    {
-        let mut cache = state.lock().await;
-        if cache.take_by_state(&query.state).is_none() {
-            return Err(StatusCode::UNAUTHORIZED);
+    let mut cache = state.lock().await;
+    let id = cache.take_by_state(&query.state);
+    match id {
+        None => {
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+        Some(id) => {
+            let application_id =
+                env::var("APPLICATION_ID").expect("APPLICATION_ID must be set in .env");
+            let app_secret = env::var("APP_SECRET").expect("APP_SECRET must be set in .env");
+
+            let client = BasicClient::new(ClientId::new(application_id))
+                .set_client_secret(ClientSecret::new(app_secret))
+                .set_redirect_uri(
+                    RedirectUrl::new("http://localhost:42069/".into())
+                        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
+                )
+                .set_token_uri(
+                    TokenUrl::new("https://discord.com/api/oauth2/token".into())
+                        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
+                );
+
+            let http_client = reqwest::ClientBuilder::new()
+                .redirect(reqwest::redirect::Policy::none())
+                .build()
+                .expect("Client should build");
+            let token = client
+                .exchange_code(AuthorizationCode::new(query.code.clone()))
+                .request_async(&http_client)
+                .await
+                .unwrap();
+
+            let role_response = http_client
+                .get("https://discord.com/api/v10/users/@me/guilds/1225154239836192900/member")
+                .header(
+                    "Authorization",
+                    format!("Bearer {}", token.access_token().secret()),
+                )
+                .header("Accept", "application/json")
+                .send()
+                .await
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+            if !role_response.status().is_success() {
+                return Err(StatusCode::from_u16(role_response.status().as_u16())
+                    .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR));
+            }
+
+            let role_data: GuildMember = role_response
+                .json()
+                .await
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+            return Ok(format!("Welcome {:#?}", role_data));
         }
     }
-    // set oauth2 client
-    let client = BasicClient::new(ClientId::new(APPLICATION_ID.into()))
-        .set_client_secret(ClientSecret::new(APP_SECRET.into()))
-        .set_redirect_uri(
-            RedirectUrl::new("http://localhost:42069/".into())
-                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-        )
-        .set_token_uri(
-            TokenUrl::new("https://discord.com/api/oauth2/token".into())
-                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-        );
-
-    let http_client = reqwest::ClientBuilder::new()
-        .redirect(reqwest::redirect::Policy::none())
-        .build()
-        .expect("Client should build");
-    let token = client
-        .exchange_code(AuthorizationCode::new(query.code.clone()))
-        .request_async(&http_client)
-        .await
-        .unwrap();
-
-    // Create a request to Discord's user API endpoint
-    let user_response = http_client
-        .get("https://discord.com/api/v10/users/@me")
-        .header(
-            "Authorization",
-            format!("Bearer {}", token.access_token().secret()),
-        )
-        .header("Accept", "application/json")
-        .send()
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    // Check if the request was successful
-    if !user_response.status().is_success() {
-        return Err(StatusCode::from_u16(user_response.status().as_u16())
-            .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR));
-    }
-
-    let user_data: DiscordUser = user_response
-        .json()
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    let username = user_data.username;
-    db().reducers.set_name(username.clone()).unwrap();
-    // Format the response with user information
-    Ok(format!("Welcome {}", username))
 }
